02aa95ac02ad0e8cc9d4568fb8af1b0d2a56fc5a
florence449 <inpyo104@gmail.com>
2025-12-04 18:58:52 +0900

    1차 TV 수정

--- file_collecter/tmdb_data_collecter/__init__.py
+++ file_collecter/tmdb_data_collecter/__init__.py
@@ -1,12 +1,12 @@
 __version__ = "1.0.0"
 
 # 준비
-from file_collecter.tmdb_data_collecter.set_up import *
+from .set_up import *
 
 # 영화 수집
-from file_collecter.tmdb_data_collecter.collect_movie_ids import *
-from file_collecter.tmdb_data_collecter.collect_movie_details import *
+from .collect_movie_ids import *
+from .collect_movie_details import *
 
 # TV 드라마 수집
-from file_collecter.tmdb_data_collecter.collect_tv_ids import *
-from file_collecter.tmdb_data_collecter.collect_tv_details import *
+from .collect_tv_ids import *
+from .collect_tv_details import *
