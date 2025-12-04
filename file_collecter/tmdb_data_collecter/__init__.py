104f9c969367e9cc114e7fd818ee3beff104be5d
florence449 <inpyo104@gmail.com>
2025-12-04 12:48:55 +0900

    tmdb 데이터 수집 로직 정리

---
+++ file_collecter/tmdb_data_collecter/__init__.py
@@ -0,0 +1,5 @@
+VERSION = "1.0.0"
+
+from file_collecter.tmdb_data_collecter.collect_movie_ids import *
+from file_collecter.tmdb_data_collecter.collect_movie_details import *
+from file_collecter.tmdb_data_collecter.set_up import *
