05ed65f4ba9d81ba98f647f85396b402da8fd79b
florence449 <inpyo104@gmail.com>
2025-12-22 15:28:04 +0900

    TF-IDF 에서 비 델타 워드클라우드 추가 및 드라마 분석을 위한 오류 수정(하드코딩 수정)

--- preprocessing/overview_vectorizer/set_up.py
+++ preprocessing/overview_vectorizer/set_up.py
@@ -4,5 +4,5 @@ import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer, ENGLISH_STOP_WORDS
 
-HIT_FILE_PATH = 'files/final_files/09_hit_score.parquet'
-MAIN_FILE_PATH = 'files/final_files/00_movie_main.parquet'
+HIT_FILE_PATH = 'files/final_files/00_hit_score.parquet'
+MAIN_FILE_PATH = 'files/final_files/movie/00_movie_main.parquet'
